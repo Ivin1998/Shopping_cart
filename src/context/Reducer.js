45e725900eb875d3a25668b24e38dd16a6a78404
Ivin1998 <ivin.austan@gmail.com>
2023-07-08 17:51:16 +0530

    testing

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -5,7 +5,6 @@ export const cartReducer=(state,action)=>{
         case "REMOVE_FROM_CART":
             return {
                 ...state,
-                
                 cart: state.cart.filter((c)=>c.id !==action.payload.id),
             };
         default:
